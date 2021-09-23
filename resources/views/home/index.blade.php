2280b4ed871e0c8734b950a8b6df42b60c44c931
Kevin Ochieng <7-kochieng@users.noreply.github.wananchi.com>
2021-09-23 12:16:14 +0300

    Login Video

--- resources/views/home/index.blade.php
+++ resources/views/home/index.blade.php
@@ -51,9 +51,13 @@ Welcome
             <div class="col-lg-6">
 
 
-                <div class="header" style=" padding-right: 100px">
+                <div class="header" style=" padding-right: 50px">
                     <br />
+
+                    <h6>Utalanta links your Talent & Skills with Opportunities. Also
+                        find Tasks & Jobs that best match your skills </h6>
                     <p class="lead">Login to Your Account</p>
+
 
                     <div class="form-group">
                         <label for="signup-email" class="control-label">Email or Telephone</label>
@@ -85,12 +89,73 @@ Welcome
 
             <div class="col-lg-6">
                 <br />
-                <h3 style="font-size:26px">Where your Talent &amp; Skills Pays Off</h3>
-                <h6>Utalanta links your Talent and skills with Opportunities from individuals or organizations. Also
-                    find Tasks & Jobs that best match your skills </h6>
+
+
+
+                <img src="/images/intro_video_artwork.jpg" class="img-fluid rounded">
+                {{-- <h3 style="font-size:26px">Where your Talent &amp; Skills Pays Off</h3> --}}
             </div>
         </div>
         <br />
+
+        {{-- <div class="container">
+            <h1>Play YouTube or Vimeo Videos in Bootstrap 4 Modal</h1>
+
+
+            <!-- Button trigger modal -->
+            <button type="button" class="btn btn-primary video-btn" data-toggle="modal"
+                data-src="https://www.youtube.com/embed/Jfrjeg26Cwk" data-target="#myModal">
+                Play Video 1 - autoplay
+            </button>
+
+            <!-- Button trigger modal -->
+            <button type="button" class="btn btn-primary video-btn" data-toggle="modal"
+                data-src="https://www.youtube.com/embed/IP7uGKgJL8U" data-target="#myModal">
+                Play Video 2
+            </button>
+
+
+            <!-- Button trigger modal -->
+            <button type="button" class="btn btn-primary video-btn" data-toggle="modal"
+                data-src="https://www.youtube.com/embed/A-twOC3W558" data-target="#myModal">
+                Play Video 3
+            </button>
+
+
+            <!-- Button trigger modal -->
+            <button type="button" class="btn btn-primary video-btn" data-toggle="modal"
+                data-src="https://player.vimeo.com/video/58385453?badge=0" data-target="#myModal">
+                Play Vimeo Video
+            </button>
+
+            <!-- Modal -->
+            <div class="modal fade" id="myModal" tabindex="-1" role="dialog" aria-labelledby="exampleModalLabel"
+                aria-hidden="true">
+                <div class="modal-dialog" role="document">
+                    <div class="modal-content">
+
+
+                        <div class="modal-body">
+
+                            <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+                                <span aria-hidden="true">&times;</span>
+                            </button>
+                            <!-- 16:9 aspect ratio -->
+                            <div class="embed-responsive embed-responsive-16by9">
+                                <iframe class="embed-responsive-item" src="" id="video" allowscriptaccess="always"
+                                    allow="autoplay"></iframe>
+                            </div>
+
+
+                        </div>
+
+                    </div>
+                </div>
+            </div>
+
+
+
+        </div> --}}
     </div>
 
 </div>
@@ -104,6 +169,30 @@ Welcome
 
 @section('css-scripts')
 <link href="dist/css/custom.css" rel="stylesheet">
+<style>
+    .modal-dialog {
+        max-width: 800px;
+        margin: 30px auto;
+    }
+
+
+
+    .modal-body {
+        position: relative;
+        padding: 0px;
+    }
+
+    .close {
+        position: absolute;
+        right: -30px;
+        top: 0;
+        z-index: 999;
+        font-size: 2rem;
+        font-weight: normal;
+        color: #fff;
+        opacity: 1;
+    }
+</style>
 
 @endsection
 
@@ -112,7 +201,43 @@ Welcome
 {{-- <script src="{{ asset('js/jquery-ui.min.js')}}"></script> --}}
 
 <script>
-    console.log("Hi theere");
+    $(document).ready(function() {
+
+// Gets the video src from the data-src on each button
+
+var $videoSrc;  
+$('.video-btn').click(function() {
+    $videoSrc = $(this).data( "src" );
+});
+console.log($videoSrc);
+
+  
+  
+// when the modal is opened autoplay it  
+$('#myModal').on('shown.bs.modal', function (e) {
+    
+// set the video src to autoplay and not to show related video. Youtube related video is like a box of chocolates... you never know what you're gonna get
+$("#video").attr('src',$videoSrc + "?autoplay=1&amp;modestbranding=1&amp;showinfo=0" ); 
+})
+  
+
+
+// stop playing the youtube video when I close the modal
+$('#myModal').on('hide.bs.modal', function (e) {
+    // a poor man's stop video
+    $("#video").attr('src',$videoSrc); 
+}) 
+    
+    
+
+
+  
+  
+// document ready  
+});
+
+
+
 </script>
 
 @endsection

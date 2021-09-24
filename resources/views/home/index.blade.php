a1b91b802ef18809cc2f64e1e919889419c86f67
Kevin Ochieng <7-kochieng@users.noreply.github.wananchi.com>
2021-09-24 11:28:34 +0300

    login page beta

--- resources/views/home/index.blade.php
+++ resources/views/home/index.blade.php
@@ -74,12 +74,12 @@ Welcome
                     <button type="submit" class="btn btn-warning btn-block"><strong>LOGIN</strong></button>
                     <div class="bottom">
                         <br>
-                        <span class="helper-text">Cant Remember Password? <a
-                                href="http://127.0.0.1:8000/authentication/login">
+                        <p> <span class="helper-text">Cant Remember Password? <a
+                                    href="http://127.0.0.1:8000/authentication/login">
 
-                                <strong> Reset </strong></a></span>
+                                    <strong> Reset </strong></a></span></p>
 
-                        <br />
+
 
                         <a class="btn btn-secondary btn-block" href="http://127.0.0.1:8000/register">Register
                             Account</a>
@@ -90,74 +90,70 @@ Welcome
             <div class="col-lg-6">
                 <br />
 
-
-
-                <img src="/images/intro_video_artwork.jpg" class="img-fluid rounded">
+                <div style="padding-top: 20px">
+                    <a href="#introVideoModal" class="video-btn" data-toggle="modal"
+                        data-src="https://www.youtube.com/embed/Jfrjeg26Cwk" data-target="#introVideoModal">
+                        <img src="/images/intro_video_artwork.jpg" class="img-fluid rounded">
+                    </a>
+                </div>
                 {{-- <h3 style="font-size:26px">Where your Talent &amp; Skills Pays Off</h3> --}}
             </div>
         </div>
         <br />
+        <br />
 
-        {{-- <div class="container">
-            <h1>Play YouTube or Vimeo Videos in Bootstrap 4 Modal</h1>
-
-
-            <!-- Button trigger modal -->
-            <button type="button" class="btn btn-primary video-btn" data-toggle="modal"
-                data-src="https://www.youtube.com/embed/Jfrjeg26Cwk" data-target="#myModal">
-                Play Video 1 - autoplay
-            </button>
-
-            <!-- Button trigger modal -->
-            <button type="button" class="btn btn-primary video-btn" data-toggle="modal"
-                data-src="https://www.youtube.com/embed/IP7uGKgJL8U" data-target="#myModal">
-                Play Video 2
-            </button>
-
-
-            <!-- Button trigger modal -->
-            <button type="button" class="btn btn-primary video-btn" data-toggle="modal"
-                data-src="https://www.youtube.com/embed/A-twOC3W558" data-target="#myModal">
-                Play Video 3
-            </button>
-
-
-            <!-- Button trigger modal -->
-            <button type="button" class="btn btn-primary video-btn" data-toggle="modal"
-                data-src="https://player.vimeo.com/video/58385453?badge=0" data-target="#myModal">
-                Play Vimeo Video
-            </button>
-
-            <!-- Modal -->
-            <div class="modal fade" id="myModal" tabindex="-1" role="dialog" aria-labelledby="exampleModalLabel"
-                aria-hidden="true">
-                <div class="modal-dialog" role="document">
-                    <div class="modal-content">
-
-
-                        <div class="modal-body">
-
-                            <button type="button" class="close" data-dismiss="modal" aria-label="Close">
-                                <span aria-hidden="true">&times;</span>
-                            </button>
-                            <!-- 16:9 aspect ratio -->
-                            <div class="embed-responsive embed-responsive-16by9">
-                                <iframe class="embed-responsive-item" src="" id="video" allowscriptaccess="always"
-                                    allow="autoplay"></iframe>
-                            </div>
-
-
-                        </div>
-
-                    </div>
-                </div>
-            </div>
-
-
-
-        </div> --}}
     </div>
 
+    <div class="container">
+
+        <div class="footer"
+            style="border-top: 1px solid #dee2e6; padding-top:20px; margin-top:50px; padding-bottom:50px">
+
+
+            <div class="row">
+                <div class="col-lg-6">
+                    <p>All Rights Reserved | Copyright 2020 © Utalanta.com</p>
+                </div>
+
+
+                <div class="col-lg-6">
+                    <div class="float-right">
+                        <a href="">About</a> &nbsp; <a href="">Privacy Statement</a> &nbsp; <a href="">Contact Us</a>
+                        &nbsp;&nbsp;
+                        Powered By <a href="http://softtouchtechnologies.com/">Soft Touch Technologies.</a>
+                    </div>
+                </div>
+
+            </div>
+        </div>
+
+    </div>
+
+</div>
+
+
+<div class=" modal fade" id="introVideoModal" tabindex="-1" role="dialog" aria-labelledby="exampleModalLabel"
+    aria-hidden="true">
+    <div class="modal-dialog" role="document">
+        <div class="modal-content">
+
+
+            <div class="modal-body">
+
+                <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+                    <span aria-hidden="true">&times;</span>
+                </button>
+                <!-- 16:9 aspect ratio -->
+                <div class="embed-responsive embed-responsive-16by9">
+                    <iframe class="embed-responsive-item" src="" id="video" allowscriptaccess="always"
+                        allow="autoplay"></iframe>
+                </div>
+
+
+            </div>
+
+        </div>
+    </div>
 </div>
 {{-- </div> --}}
 
@@ -202,41 +198,21 @@ Welcome
 
 <script>
     $(document).ready(function() {
+        var $videoSrc;  
+        $('.video-btn').click(function() {
+            $videoSrc = $(this).data( "src" );
+        });
 
-// Gets the video src from the data-src on each button
-
-var $videoSrc;  
-$('.video-btn').click(function() {
-    $videoSrc = $(this).data( "src" );
-});
-console.log($videoSrc);
-
-  
-  
-// when the modal is opened autoplay it  
-$('#myModal').on('shown.bs.modal', function (e) {
-    
-// set the video src to autoplay and not to show related video. Youtube related video is like a box of chocolates... you never know what you're gonna get
-$("#video").attr('src',$videoSrc + "?autoplay=1&amp;modestbranding=1&amp;showinfo=0" ); 
-})
-  
+        $('#introVideoModal').on('shown.bs.modal', function (e) {
+             $("#video").attr('src',$videoSrc + "?autoplay=1&amp;modestbranding=1&amp;showinfo=0" ); 
+        })
+        
+        $('#introVideoModal').on('hide.bs.modal', function (e) {
+            $("#video").attr('src',$videoSrc); 
+        }) 
 
 
-// stop playing the youtube video when I close the modal
-$('#myModal').on('hide.bs.modal', function (e) {
-    // a poor man's stop video
-    $("#video").attr('src',$videoSrc); 
-}) 
-    
-    
-
-
-  
-  
-// document ready  
-});
-
-
+    });
 
 </script>
 

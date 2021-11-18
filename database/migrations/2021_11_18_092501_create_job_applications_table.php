4086934c88bb8c9b070beeb92005a768355de33b
roy.star <royemw@gmail.com>
2021-11-18 13:23:11 +0300

    more seeders and tables

---
+++ database/migrations/2021_11_18_092501_create_job_applications_table.php
@@ -0,0 +1,38 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateJobApplicationsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('job_applications', function (Blueprint $table) {
+            $table->id();
+            $table->bigInteger('user_id')->nullable($value = true);
+            $table->bigInteger('job_id')->nullable($value = true);
+            $table->text('resume_upload')->nullable($value = true);
+            $table->text('cover_letter')->nullable($value = true);
+            $table->text('other_attachments')->nullable($value = true);
+            $table->integer('created_by');
+            $table->integer('updated_by');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('job_applications');
+    }
+}

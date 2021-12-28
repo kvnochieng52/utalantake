d05e6ef07b24c057bdca3b26f6634796141d5786
Kevin Ochieng <7-kochieng@users.noreply.github.wananchi.com>
2021-12-28 12:22:38 +0300

    Active 1 currencies

--- database/seeds/CurrenciesSeeder.php
+++ database/seeds/CurrenciesSeeder.php
@@ -1,6 +1,7 @@
 <?php
 
 use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\DB;
 
 class CurrenciesSeeder extends Seeder
 {
@@ -14,13 +15,13 @@ class CurrenciesSeeder extends Seeder
         DB::table('currencies')->insert([
             [
                 'currency_name' => 'KES',
-                'visible' => 1,
+                'active' => 1,
                 'created_by' => 1,
                 'updated_by' => 1,
             ],
             [
                 'currency_name' => 'USD',
-                'visible' => 1,
+                'active' => 1,
                 'created_by' => 1,
                 'updated_by' => 1,
             ]
